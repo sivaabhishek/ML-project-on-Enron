366878f74a6ca3d40cad567521d21ea02e0db853
S. Siva Abhishek <60132530+sivaabhishek@users.noreply.github.com>
2020-07-31 14:32:44 +0530

    Update startup.py

--- tools/startup.py
+++ tools/startup.py
@@ -22,15 +22,15 @@ try:
 except:
     print("you should install sklearn before continuing")
 
-# print()
-# print("downloading the Enron dataset (this may take a while)")
-# print("to check on progress, you can cd up one level, then execute <ls -lthr>")
-# print("Enron dataset should be last item on the list, along with its current size")
-# print("download will complete at about 423 MB")
-# import urllib
-# url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
-# urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz")
-# print("download complete!")
+print()
+print("downloading the Enron dataset (this may take a while)")
+print("to check on progress, you can cd up one level, then execute <ls -lthr>")
+print("Enron dataset should be last item on the list, along with its current size")
+print("download will complete at about 423 MB")
+import urllib
+url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
+urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz")
+print("download complete!")
 
 
 print()
@@ -38,7 +38,6 @@ print("unzipping Enron dataset (this may take a while)")
 import tarfile
 import os
 os.chdir("..")
-# files='C:\\Users\\sivaa\\PycharmProjects\\infytq\\ML\\ud120-projects-master\\enron_mail_20150507.tar.gz'
 tfile = tarfile.open("enron_mail_20150507.tar.gz")
 # tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
 tfile.extractall()
